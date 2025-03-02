9d9fb971582d84923b695a6074a6486885169627
Menacedbydennis <vince.hale@g.austincc.edu>
2025-03-02 05:27:32 +0000

    added clock changes  vdh

--- src/homework/e_functions/main.py
+++ src/homework/e_functions/main.py
@@ -1,1 +1,8 @@
-#
+from src.homework.e_functions.value_return import get_hour, get_minutes, get_seconds
+
+epoch_seconds = 3800
+hours = get_hour(epoch_seconds)
+minutes = get_minutes(epoch_seconds)
+seconds = get_seconds(epoch_seconds)
+
+print(f"Time: {hours:02}:{minutes:02}:{seconds:02}")  # Output should be 01:03:20
